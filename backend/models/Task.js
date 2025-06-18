87a363eee1370d70f58340c9a5b93a82bd6d0361
Rupanjana Majhi <rup210works@gmail.com>
2025-06-19 04:06:57 +0530

    defining mongodb schema

---
+++ backend/models/Task.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const TodoSchema = new mongoose.Schema({
+    text: { type: String, required: true },
+    completed: { type: Boolean, default: false },
+});
+
+const TaskSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    description: { type: String },
+    priority: { type: String, enum: ['low', 'medium', 'high'], default: 'medium' },
+    status: { type: String, enum: ['todo', 'in-progress', 'done'], default: 'todo' },
+    dueDate: { type: Date, required: true },
+    assignedTo: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+    createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    attachments: [{ type: String }],
+    todoChecklist: [TodoSchema],
+    progress: { type: Number, default: 0 }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Task', TaskSchema);
